5a8f68bba20e09170a1ca706379ea162c5d2a289
LazyPlanet <1211618464@qq.com>
2018-03-11 13:41:17 +0800

    优化代码

--- GameServer/Main.cpp
+++ GameServer/Main.cpp
@@ -122,12 +122,14 @@ int main(int argc, const char* argv[])
 		//signals.async_wait(SignalHandler);
 		//
 
+		/*
 		std::string server_ip = ConfigInstance.GetString("ServerIP", "0.0.0.0");
 		if (server_ip.empty()) return 4;
 		
 		int32_t server_port = ConfigInstance.GetInt("ServerPort", 50001);
 		if (server_port <= 0 || server_port > 0xffff) return 5;
-		
+		*/
+
 		int32_t thread_count = ConfigInstance.GetInt("ThreadCount", 5);
 		if (thread_count <= 0) return 6;
 
