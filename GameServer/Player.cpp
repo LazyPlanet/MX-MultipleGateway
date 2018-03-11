2be8068bf7e7107a1878d49fba1939309aecf034
LazyPlanet <1211618464@qq.com>
2018-03-11 15:03:16 +0800

    会牌幺九

--- GameServer/Player.cpp
+++ GameServer/Player.cpp
@@ -2131,7 +2131,7 @@ bool Player::CheckHuiHu(const Asset::PaiElement& pai, bool check_zimo, bool calc
 
 	if (!_room->HasHuiPai()) return false; //不带会儿牌
 
-	if (!HasYaoJiu()) return false; //会牌不能代替幺九,因此前置检查
+	//if (!HasYaoJiu()) return false; //会牌不能代替幺九,因此前置检查
 
 	int32_t count = GetHuiPaiCount();
 
@@ -2178,6 +2178,8 @@ bool Player::CheckHuiHu(const Asset::PaiElement& pai, bool check_zimo, bool calc
 	}
 	
 	if (count == 0) return false; //没有会牌，也没有绝头会儿
+	
+	if (!HasYaoJiu(cards_inhand, cards_outhand, minggang, angang, jiangang, fenggang)) return false; //幺九检查
 
 	DEBUG("玩家:{} 拥有会儿牌数量:{}", _player_id, count);
 
